e62d7b9d7329394745c0c95b8fac0de7549b7c72
KhalidPC <khalid.ahmad063@gmail.com>
2023-06-06 14:53:08 +0600

    First Commit

---
+++ android/app/src/main/kotlin/com/codevillage/module_5_assignment/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.codevillage.module_5_assignment
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Assignment Five',
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget{
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Assignment 5'),
+      ),
+    );
+  }
+
+}
+
